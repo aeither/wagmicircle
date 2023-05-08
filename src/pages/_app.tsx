32731fcf0bb529181d4c39b577c295630b4fd8a1
aeither <36173828+aeither@users.noreply.github.com>
2023-05-08 11:13:19 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,11 @@
 import { WalletEntryPosition } from "@particle-network/auth";
 import {
-  Avalanche,
   BSC,
   BSCTestnet,
   Ethereum,
   EthereumGoerli,
-  KCCTestnet,
-  Moonbeam,
-  Moonriver,
-  Optimism,
-  PlatON,
-  Polygon,
-  Solana,
 } from "@particle-network/common";
-import { evmWallets, solanaWallets } from "@particle-network/connect";
+import { evmWallets } from "@particle-network/connect";
 import { ModalProvider } from "@particle-network/connect-react-ui";
 import "@particle-network/connect-react-ui/esm/index.css";
 import { type AppType } from "next/dist/shared/lib/utils";
@@ -39,26 +31,13 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         projectId: env.NEXT_PUBLIC_PROJECT_ID,
         clientKey: env.NEXT_PUBLIC_CLIENT_KEY,
         appId: env.NEXT_PUBLIC_APP_ID,
-        chains: [
-          PlatON,
-          Optimism,
-          Moonbeam,
-          Moonriver,
-          Avalanche,
-          Polygon,
-          BSC,
-          Ethereum,
-          EthereumGoerli,
-          Solana,
-          BSCTestnet,
-          KCCTestnet,
-        ],
+        chains: [BSC, BSCTestnet],
         particleWalletEntry: {
           displayWalletEntry: true,
           defaultWalletEntryPosition: WalletEntryPosition.BR,
           supportChains: [Ethereum, EthereumGoerli],
         },
-        wallets: [...evmWallets({ qrcode: false }), ...solanaWallets()],
+        wallets: [...evmWallets({ qrcode: false })],
       }}
       language="en"
       theme={"light"}
--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,28 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
-import { ConnectButton } from "@particle-network/connect-react-ui";
-import { useAccount } from "@particle-network/connect-react-ui";
+import {
+  ConnectButton,
+  useAccount,
+  useConnectKit,
+} from "@particle-network/connect-react-ui";
 
 export default function Home() {
   //use this in react component.
   const account = useAccount();
-  if (account) {
-    // connect wallet success
-  }
+  const connectKit = useConnectKit();
+  //   const userInfo = connectKit.particle.auth.userInfo();
+  //   console.log("🚀 ~ file: connect.tsx:12 ~ Home ~ userInfo:", userInfo)
 
   return (
-    <div>
-      <main>
-        <h1>
-          <a href="https://docs.particle.network/connect-service/sdks/web">
-            Particle Connect Docs
-          </a>
-          {account && <div>{account}</div>}
-        </h1>
-        <div style={{ marginTop: 100 }}>
-          <ConnectButton />
-        </div>
-      </main>
-    </div>
+    <main className="min-h-[calc(100vh-64px)]">
+      <h1>
+        <a href="https://docs.particle.network/connect-service/sdks/web">
+          Particle Connect Docs
+        </a>
+        {account && <div>{account}</div>}
+      </h1>
+      <div className="flex h-48 w-full p-16">
+        <ConnectButton />
+      </div>
+    </main>
   );
 }
